789f3bae2161d1fda9f9e6ea203113712676cbcc
Michel Bras <michelbras1990@gmail.com>
2021-07-11 17:21:06 +0200

    initialize react with routing

---
+++ src/pages/Foo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Foo(): React.ReactElement {
+  return (
+    <div>
+      <p>Foo</p>
+    </div>
+  );
+}
+
+export default Foo;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home(): React.ReactElement {
+  return (
+    <div>
+      <p>Home</p>
+    </div>
+  );
+}
+
+export default Home;
